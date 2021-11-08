58169079dad20917b487e1433c61e6bb8f483db3
albertlorenzo <albertlorenzofc@gmail.com>
2021-11-08 12:26:04 +0100

    first

---
+++ P1/procesado_imagen_p1.c
@@ -0,0 +1,159 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+void bubble_sort(int array[], int size)
+{
+    for (int i = 0; i < size - 1; i++)
+    {
+        for (int j = 0; j < size - i - 1; j++)
+        {
+            if (array[j] > array[i + 1])
+            {
+                int aux = array[j];
+                array[j] = array[i];
+                array[i] = aux;
+            }
+        }
+    }
+}
+
+// Se declara y reserva memoria cualquier matriz
+void init_matrix(unsigned char ***M_ptr, int width, int height)
+{
+    unsigned char **matrix;
+    matrix = (unsigned char **)malloc(sizeof(unsigned char *) * height);
+
+    for (int i = 0; i < width; i++)
+    {
+        matrix[i] = (unsigned char *)malloc(sizeof(unsigned char) * height);
+    }
+
+    *M_ptr = matrix;
+}
+
+// Función para inicializar y copiar los datos del fichero .raw en una matriz
+void read_binary_data(unsigned char **matrix, const char *input_file, int width, int height)
+{
+    FILE *raw_data;
+
+    raw_data = fopen(input_file, "rb");
+
+    for (int i = 1; i < height + 1; ++i)
+    {
+        fread(matrix[i] + 1, sizeof(unsigned char), width, raw_data);
+    }
+
+    fclose(raw_data);
+}
+
+// Vuelca los datos en un fichero .raw con formato binario
+void write_binary_data(unsigned char *array, const char *output_file, int width, int height)
+{
+    FILE *output;
+
+    output = fopen(output_file, "wb");
+
+    fwrite(array, sizeof(unsigned char), width * height, output);
+
+    fclose(output);
+}
+
+void padding(unsigned char **matrix, int width, int height)
+{
+    // Esquina izquierda superior
+    matrix[0][0] = matrix[2][2];
+
+    // Esquina derecha superior
+    matrix[0][width + 1] = matrix[2][width - 1];
+
+    // Esquina izquierda inferior
+    matrix[height + 1][0] = matrix[height - 1][2];
+
+    // Esquina derecha inferior
+    matrix[height + 1][width + 1] = matrix[height - 1][width - 1];
+
+    // Fila superior
+    for (int i = 1; i <= width; i++)
+    {
+        matrix[0][i] = matrix[2][i];
+    }
+
+    // Fila inferior
+    for (int i = 1; i <= width; i++)
+    {
+        matrix[height + 1][i] = matrix[height - 1][i];
+    }
+
+    // Columna izquierda
+    for (int i = 1; i <= height; i++)
+    {
+        matrix[i][0] = matrix[i][2];
+    }
+
+    // Columna derecha
+    for (int i = 0; i <= height; i++)
+    {
+        matrix[i][width + 1] = matrix[i][width - 1];
+    }
+}
+
+void sobel_filter(unsigned char **matrix, unsigned char *result, int width, int height)
+{
+    int Gx[9] = {-1, 0, 1, -2, 0, 2, -1, 0, 1}, Gy[9] = {-1, -2, -1, 0, 0, 0, 1, 2, 1};
+    unsigned char data[9];
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int gx_sum = 0, gy_sum = 0;
+
+            data[0] = matrix[i][j];
+            data[1] = matrix[i][j + 1];
+            data[2] = matrix[i][j + 2];
+            data[3] = matrix[i + 1][j];
+            data[4] = matrix[i + 1][j + 1];
+            data[5] = matrix[i + 1][j + 2];
+            data[6] = matrix[i + 2][j];
+            data[7] = matrix[i + 2][j + 1];
+            data[8] = matrix[i + 2][j + 2];
+
+            for (int k = 0; k < 9; k++)
+            {
+                gx_sum += data[k] * Gx[k];
+                gy_sum += data[k] * Gy[k];
+            }
+
+            // Esta parte del código es opcional ya que sirve para reducir la intensidad de los bordes
+            gx_sum /= 4;
+            gy_sum /= 4;
+
+            int processed_bit = sqrt(gx_sum * gx_sum + gy_sum * gy_sum);
+
+            result[i * width + j] = processed_bit;
+        }
+    }
+}
+
+// Los parámetros recogidos son: anchura, altura, fichero de entrada, fichero de salida y filtro. No hay validación de errores.
+int main(int argc, char *argv[])
+{
+    int width = atoi(argv[1]), height = atoi(argv[2]), filter = atoi(argv[5]);
+    char *input_file = argv[3], *output_file = argv[4];
+
+    unsigned char **matrix, result[width * height];
+
+    init_matrix(&matrix, width + 2, height + 2);
+
+    read_binary_data(matrix, input_file, width, height);
+
+    //padding(matrix, width, height);
+
+    sobel_filter(matrix, result, width, height);
+
+    write_binary_data(result, output_file, width, height);
+
+    return 0;
+}
---
+++ P1/sistema_iterativo_p1.c
@@ -0,0 +1,157 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define N 15000
+
+void write_matrix_data(double **matrix)
+{
+    FILE *output;
+
+    output = fopen("matriz.txt", "w");
+
+    for (int i = 0; i < N; i++)
+    {
+        fwrite(matrix[i], sizeof(double), N, output);
+    }
+
+    fclose(output);
+}
+// Función para copiar valores de un array a otro
+void copy_vector_values(double vec_a[], double vec_b[])
+{
+    for (int i = 0; i < N; i++)
+    {
+        vec_b[i] = vec_a[i];
+    }
+}
+
+// Convierte un valor con signo a absoluto mediante un operador ternario
+double absolute(double n) {
+    return n > 0 ? n : -n;
+}
+
+// Encuentra el mayor valor absoluto en un vector y devuelve el índice en el que se encuentra
+int find_max_absolute(double vec[])
+{
+    double max = absolute(vec[0]), current = 0;
+    int position = 0;
+    for (int i = 1; i < N; i++)
+    {
+        current = absolute(vec[i]);
+        if (current > max)
+        {
+            position = i;
+            max = current;
+        }
+    }
+    return position;
+}
+
+// Divido cada componente de un vector por el parámetro max
+void divide(double vec[], double max)
+{
+    for (int i = 0; i < N; i++)
+    {
+        vec[i] /= max;
+    }
+}
+
+// Inicialización de una matriz mediante punteros
+void init_matrix(double ***M_ptr)
+{
+    // Se declara y reserva memoria para la matriz
+    double **matrix = (double**)malloc(sizeof(double *) * N);
+
+    for (int i = 0; i < N; ++i)
+    {
+        matrix[i] = (double *)malloc(sizeof(double) * N);
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            // Diagonal matriz
+            if (j == i)
+            {
+                matrix[i][j] = 1;
+            }
+            // Mitad triangular superior
+            else if (i > j)
+            {
+                matrix[i][j] = (double)-50 * (i + 1) * (j + 1) / (N + N);
+            }
+            // Mitad triangular inferior
+            else
+            {
+                matrix[i][j] = (double)50 * (i + 1) * (j + 1) / (N + N);
+            }
+        }
+    }
+
+    *M_ptr = matrix;
+}
+
+// Inicialización de un vector mediante punteros, en este caso, el vector unidad
+void init_unity_vector(double *x0_ptr[])
+{
+    double *vector_aux = (double *)malloc(sizeof(double) * N);
+    for (int i = 0; i < N; i++)
+    {
+        vector_aux[i] = 1;
+    }
+    *x0_ptr = vector_aux;
+}
+
+// Función para iniciar el sistema iterativo
+void init_system(double **M, double x0[], int m)
+{
+
+    double vector_aux[N], sum;
+
+    for (int k = 0; k < m; k++)
+    {
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                // Multiplico el valor de la matriz por la componente del vector
+                sum += M[i][j] * x0[j];
+            }
+            // Actualizo la componente del vector y limpio el valor de la suma, así en una nueva iteración no arrastro el valor de la componente antigua
+            vector_aux[i] = sum;
+            sum = 0;
+        }
+
+        if (k > 0)
+        {
+            // Encuentra y devuelve el índice de la componente con un mayor valor absoluto dentro de un vector
+            int position = find_max_absolute(vector_aux);
+
+            // dividir los valores del vector por el máximo absoluto, respetando el signo original
+            divide(vector_aux, vector_aux[position]);
+        }
+
+        // Copio los valores del vector resultante en el vector inicial para que afecte a las siguientes iteraciones
+        copy_vector_values(vector_aux, x0);
+    }
+
+}
+
+int main(int argc, char *argv[])
+{
+    double **M;
+    double *x0; 
+    int m = atoi(argv[1]);
+
+    init_matrix(&M);
+
+    init_unity_vector(&x0);
+
+    init_system(M, x0, m);
+
+    write_matrix_data(M);
+
+    return 0;
+}
